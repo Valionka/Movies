4ad794150dfcab4731e71e435935fbc97895d9f7
Val <vmiha@yahoo-inc.com>
2016-09-12 13:32:48 -0700

    Initial Commit

---
+++ Movies/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Movies
+//
+//  Created by Valentin Mihaylov on 9/12/16.
+//  Copyright © 2016 codepath. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Movies/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Movies
+//
+//  Created by Valentin Mihaylov on 9/12/16.
+//  Copyright © 2016 codepath. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
